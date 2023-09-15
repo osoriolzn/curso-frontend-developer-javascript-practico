a8f7fae79271729c3ef0c6583bfacfbc292c2cde
Yohan D Osorio L <yohan.osorio@arroyoconsulting.net>
2023-09-15 18:45:46 -0500

    Agregar funcionalidad para ver detalle del prod.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const menuHanIcon = document.querySelector('.menu')
+const productDetailIcon = document.querySelector('.product-detail-close')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuCarIcon = document.querySelector('.navbar-shopping-cart')
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
+const porductDetailContainer = document.querySelector('#productDetail')
 const cardsContainer = document.querySelector('.cards-container')
-
-menuEmail.addEventListener('click', toggleDesktopMenu)
-menuHanIcon.addEventListener('click', toggleMobileMenu)
-menuCarIcon.addEventListener('click', toggleCarAside)
+const productList = []
 
 function toggleDesktopMenu() {
     const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
@@ -27,12 +26,23 @@ function toggleMobileMenu() {
         shoppingCartContainer.classList.add('inactive')
     }
 
+    closeProductAside()
     mobileMenu.classList.toggle('inactive')
+}
+
+function openProductAside() {
+    shoppingCartContainer.classList.add('inactive')
+    porductDetailContainer.classList.remove('inactive')
+}
+
+function closeProductAside() {
+    porductDetailContainer.classList.add('inactive')
 }
 
 function toggleCarAside() {
     const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
     const isDesktopMenuClosed = desktopMenu.classList.contains('inactive')
+    const isProductDetailClosed = porductDetailContainer.classList.contains('inactive')
 
     if (!isMobileMenuClosed) {
         mobileMenu.classList.add('inactive')
@@ -40,11 +50,12 @@ function toggleCarAside() {
     else if (!isDesktopMenuClosed) {
         desktopMenu.classList.add('inactive')
     }
+    else if (!isProductDetailClosed) {
+        porductDetailContainer.classList.add('inactive')
+    }
 
     shoppingCartContainer.classList.toggle('inactive')
 }
-
-const productList = []
 
 function renderProduct(object) {
     for (product of object) {
@@ -53,6 +64,7 @@ function renderProduct(object) {
     
         const productImg = document.createElement('img')
         productImg.setAttribute('src', product.image)
+        productImg.addEventListener('click', openProductAside)
     
         const productInfo = document.createElement('div')
         productInfo.classList.add('product-info')
@@ -82,6 +94,11 @@ function renderProduct(object) {
         cardsContainer.appendChild(productCard)
     }
 }
+
+menuEmail.addEventListener('click', toggleDesktopMenu)
+menuHanIcon.addEventListener('click', toggleMobileMenu)
+menuCarIcon.addEventListener('click', toggleCarAside)
+productDetailIcon.addEventListener('click', closeProductAside)
 
 productList.push({
     name: 'Bike',
